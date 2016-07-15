c2816be17f1237ef138614f58db6339988aadea5
noam finkelstein <noam.finkelstein@gmail.com>
2016-07-15 16:37:54 -0400

    stub

--- R/assertions.R
+++ R/assertions.R
@@ -12,3 +12,17 @@ assert_true = function(expr) {
     }
 }
 
+stub = function(func, to_stub, return_value) {
+    original_func = get(func, envir=parent.frame(), mode='function')
+
+    env = environment(original_func)
+    assign(to_stub, function(...) return(return_value), env)
+
+    function_output = capture.output(print(original_func))
+    function_vector = head(function_output, -1)  # drop environment from output
+    function_text = paste(function_vector, set='', collapse='')
+
+    new_function = eval(parse(text=function_text), env)
+    assign(func, new_function, parent.frame())
+}
+
--- tests/proctor_test.R
+++ tests/proctor_test.R
@@ -5,3 +5,10 @@ test_pass = function() {
 test_failure = function() {
     proctor::assert_equal(200, 100)
 }
+
+test_stub = function() {
+    f = function() 10
+    g = function() f()
+    proctor::stub('g', 'f', 20)
+    proctor::assert_equal(g(), 20)
+}
