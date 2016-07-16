ae97814ba288115df9f0e52854445e1cb6f596fd
noam finkelstein <noam.finkelstein@gmail.com>
2016-07-16 10:20:43 -0400

    improve stubbing

--- R/assertions.R
+++ R/assertions.R
@@ -14,15 +14,18 @@ assert_true = function(expr) {
 
 stub = function(func, to_stub, return_value) {
     original_func = get(func, envir=parent.frame(), mode='function')
-
     env = environment(original_func)
     assign(to_stub, function(...) return(return_value), env)
 
-    function_output = capture.output(print(original_func))
-    function_vector = head(function_output, -1)  # drop environment from output
-    function_text = paste(function_vector, set='', collapse='')
-
+    function_text = _get_function_text(original_func)
     new_function = eval(parse(text=function_text), env)
     assign(func, new_function, parent.frame())
+}
+
+_get_function_text = function(func) {
+    functiin_output = capture.output(print(func))
+    function_vector = head(function_output, -1)  # drop environment from output
+    function_text = paste(function_vector, set='', collapse='')
+    return(function_text)
 }
 
--- tests/proctor_test.R
+++ tests/proctor_test.R
@@ -6,9 +6,11 @@ test_failure = function() {
     proctor::assert_equal(200, 100)
 }
 
+a = 15
+f = function() 10
+g = function() f() + a
 test_stub = function() {
-    f = function() 10
-    g = function() f()
+    proctor::assert_equal(g(), 25)
     proctor::stub('g', 'f', 20)
-    proctor::assert_equal(g(), 20)
+    proctor::assert_equal(g(), 35)
 }
