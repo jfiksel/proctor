649323c6f96507175b5decf2f164a14e58c0f055
noam finkelstein <noam.finkelstein@gmail.com>
2016-10-23 08:54:12 -0400

    remove win script

--- tests/proctor_test.R
+++ tests/proctor_test.R
@@ -6,11 +6,3 @@ test_failure = function() {
     proctor::assert_equal(200, 100)
 }
 
-a = 15
-f = function() 10
-g = function() f() + a
-test_stub = function() {
-    proctor::assert_equal(g(), 25)
-    proctor::stub('g', 'f', 20)
-    proctor::assert_equal(g(), 35)
-}
